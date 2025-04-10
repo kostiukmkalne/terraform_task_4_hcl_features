13e0925fbd4cb57fc314f80e2cf4716326edd90f
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2025-04-10 12:01:00 -0500

    Solution3

--- vm.tf
+++ vm.tf
@@ -5,7 +5,7 @@ resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm-${each.key}"
   location              = each.value
   resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main[local.nic_names[each.key]].id]
+  network_interface_ids = [azurerm_network_interface.main[each.value].id]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
